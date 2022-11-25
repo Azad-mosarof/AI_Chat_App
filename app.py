41ffbc367f8a913d73f72e550ea4b2b30aad1d27
Azad-mosarof <98151626+Azad-mosarof@users.noreply.github.com>
2022-11-26 01:48:04 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, request
+import openai
+
+openai.api_key = "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"
+
+app = Flask(__name__)
+
+@app.route("/get_answer", methods=["POST"])
+def get_answer():
+    promt = request.get_json()['prompt']
+    return jsonify(openai.Completion.create(
+    model="text-davinci-002",
+    prompt=promt,
+    temperature=0.9,
+    max_tokens=150,
+    top_p=1,
+    frequency_penalty=0.0,
+    presence_penalty=0.6,
+    stop=[" Human:", " AI:"]
+    )["choices"][0]["text"])
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
